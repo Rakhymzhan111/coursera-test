37efda89b06bcd6e0799ae3224dd1cfe49c10d00
Rakhymzhan111 <rakhymjan.zhakupoff@gmail.com>
2026-03-26 18:10:13 +0500

    Create byeSpeaker.js

---
+++ module4-solution/js/byeSpeaker.js
@@ -0,0 +1,12 @@
+javascript
+(function(window) {
+    var byeSpeaker = {};
+    var speakWord = "Goodbye";
+    
+    byeSpeaker.speak = function(name) {
+        console.log(speakWord + " " + name);
+    };
+    
+    window.byeSpeaker = byeSpeaker;
+    
+})(window)
